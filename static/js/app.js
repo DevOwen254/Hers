451d0d2df2b509e315cf912b8acd89c71421f13d
Owen Munene <muneneowen8@gmail.com>
2026-08-28 08:13:50 +0300

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,27 @@
+const messages = [
+  "You are my calm in chaos.",
+  "I choose you, always.",
+  "You feel like home.",
+  "Some feelings never fade."
+];
 
+function generate() {
+  const msg = messages[Math.floor(Math.random()*messages.length)];
+  document.getElementById("msg").innerText = msg;
+}
+
+const cards = document.querySelectorAll(".memory");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, {
+  threshold: 0.3
+});
+
+cards.forEach(card => {
+  observer.observe(card);
+});
